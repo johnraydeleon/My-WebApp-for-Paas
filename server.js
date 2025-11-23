88c765ddf9a1193c80c8e76539b62e940b0c4c8a
John Ray De Leon <91359801+johnraydeleon@users.noreply.github.com>
2025-11-23 08:59:18 +0000

    your message

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require("http");
 const PORT = process.env.PORT || 3000;
 const server = http.createServer((req ,res) => {
-    res.write("Hello Cloud!");
-    res.writeline("This is first website that have server")
+    res.write("Hello Cloud!\n");
+    res.write("This is first website that have server")
     res.end();
 });
 server.listen(PORT, () => {
