c4e1229e60e14df9a710dc4a426799821edb2134
John Ray De Leon <91359801+johnraydeleon@users.noreply.github.com>
2025-11-23 08:14:50 +0000

    package.json

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req ,res) => {
+    res.write("Hello Cloud!");
+    res.end();
+});
+server.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
